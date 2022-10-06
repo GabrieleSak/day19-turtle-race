141e5bfe74a57fa6ace97c61d6813b87c725b1fc
GabrieleSak <gabriele.sakalauskiene@yahoo.com>
2022-10-06 22:04:11 +0300

    Create as many turtles as there are colors given and place them in different y position

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from turtle import Turtle, Screen
 
-tim = Turtle()
 screen = Screen()
-
 screen.setup(width=500, height=400)
-
-
-
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color:")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_pos = 0
+for color in colors:
+    tim = Turtle(shape="turtle")
+    tim.penup()
+    tim.color(color)
+    tim.goto(x=-230, y=-100 + y_pos)
+    y_pos += 40
 
 screen.exitonclick()
